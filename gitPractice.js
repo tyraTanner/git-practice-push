3dff5186802594594e350251e1ebf04528ad4421
Tyra Tanner <tyra@Tyras-MBP.lan>
2022-05-18 13:12:44 -0600

    notes on git

---
+++ gitPractice.js
@@ -0,0 +1,20 @@
+/*
+git status: state of working directory
+git commit
+git branch
+git checkout <location> (can type any place in the git main or branches or commits)
+git checkout -b (combines branch & checkout)
+git merge
+git rebase (like merge but combines back into single linear flow)
+git log (to see hashes)
+HEAD (points to most recent commit)
+
+Relative Refs:
+  ^ means go up one in the git chain, for example, 'git checkout HEAD^' will go to HEAD's parent
+  ~<num> means go up a specified number of times, so 'git checkout HEAD~4' will go up 4 commits from HEAD
+  -f means reassign a branch to a commit, so 'git branch -f main HEAD~3' moves (by force) the main branch to three parents behind HEAD.
+
+Making Changes:
+git reset (will go back in time and make it as if a commit had never happened, so "git reset HEAD~1" will go back 1 commit. ONLY works for local branches on your machine, not on branches others are using.)
+git revert (will make a new commit with changes that just happen to reset. This allows changes to be shared with others.)
+*/
