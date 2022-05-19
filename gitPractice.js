1f22ab5e6cf04ba620b407d095d4965902953fc1
Tyra Tanner <tyra.tanner@gmail.com>
2022-05-19 10:10:58 -0600

    Cleaned up notes on git

--- gitPractice.js
+++ gitPractice.js
@@ -31,17 +31,3 @@ Once you type in "git commit", it opens vi.
     If you want to move through the editor, use "j" and "k". 
     Use ":wq" to quit and save
 */
-
-function add(a, b) {
-  return a + b;
-}
-
-/*
-Here are some comments I will now branch onto a new branch in git
-I'm gonna add some more thoughts here. 
-This is in my branch1 git branch. I'm gonna commit these there.
-*/
-
-// Now I will make a second branch called branch1 again
-// So that doesn't work, it says branch1 still exists. If it still exists, but was merged, where does it exist?
-// So now it says I'm on branch 1, so I'm going to keep typing and then make a new commit, then from there, I'll merge with master again
